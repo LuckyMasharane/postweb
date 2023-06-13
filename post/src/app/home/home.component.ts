81de1553bc0ca9e9d492439f77d96aa366050198
LuckyMasharane <lmasharane@gmail.com>
2023-06-13 20:45:44 +0200

    third

--- post/src/app/home/home.component.ts
+++ post/src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  logo =[
+    // { logo:"https://portal.thecourierguy.co.za/sites/tcg/logo.png?v=1" },
+    { logo:"https://prd-static-default-2.sf-cdn.com/resources/images/store/2015/global/640x400/Canvas/x1-640x350-20221213.jpg.pagespeed.ic.wJnVXu_sgj.jpg" },
+    
+  ]
+
+  
 }
