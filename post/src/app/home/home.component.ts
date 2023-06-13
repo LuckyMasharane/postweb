e34e664a65e54e77b032041c77f3324ac8a1293d
LuckyMasharane <lmasharane@gmail.com>
2023-06-13 21:17:16 +0200

    service

--- post/src/app/home/home.component.ts
+++ post/src/app/home/home.component.ts
@@ -12,6 +12,15 @@ export class HomeComponent implements OnInit{
 
   ngOnInit(): void {
   }
+
+  service =[
+    { serv:"https://portal.thecourierguy.co.za/sites/tcg/logo.png?v=1" },
+    { serv: "https://cdn.24.co.za/files/Cms/General/d/10808/68d1daa5d4e6478eb57a235711226395.jpg"},
+    { serv: "https://www.teamworkphoto.com/wp-content/uploads/2019/01/kodak-logo.png"}
+  ]
+  stationery =[
+    {stat: "https://cdn.24.co.za/files/Cms/General/d/10808/68d1daa5d4e6478eb57a235711226395.jpg"}
+  ]
   logo =[
     // { logo:"https://portal.thecourierguy.co.za/sites/tcg/logo.png?v=1" },
     { logo:"https://prd-static-default-2.sf-cdn.com/resources/images/store/2015/global/640x400/Canvas/x1-640x350-20221213.jpg.pagespeed.ic.wJnVXu_sgj.jpg" },
